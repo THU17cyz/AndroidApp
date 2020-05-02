895ed4c36e51b2bc99ee8c86fb9bb2c43c25c02c
Yezhen Cong <52420115+THU17cyz@users.noreply.github.com>
2020-05-02 13:13:09 +0800

    one more navigation icon

---
+++ frontend/app/src/main/java/com/example/androidapp/ui/query/QueryFragment.java
@@ -0,0 +1,35 @@
+package com.example.androidapp.ui.query;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.androidapp.R;
+import com.example.androidapp.ui.dashboard.DashboardViewModel;
+
+public class QueryFragment extends Fragment {
+    private QueryViewModel queryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        queryViewModel =
+                ViewModelProviders.of(this).get(QueryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_query, container, false);
+        final TextView textView = root.findViewById(R.id.text_query);
+        queryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
