e1b3365a41573e6156d7d653778f100b9b8cd8fc
Yezhen Cong <52420115+THU17cyz@users.noreply.github.com>
2020-05-08 00:59:14 +0800

    added front page tabLayout

---
+++ frontend/app/src/main/java/com/example/androidapp/Fragments/TabFragment1.java
@@ -0,0 +1,30 @@
+package com.example.androidapp.Fragments;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.androidapp.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabFragment1 extends Fragment {
+
+
+    //To do
+    public TabFragment1() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment1, container, false);
+    }
+    //To do closed
+}
--- frontend/app/src/main/java/com/example/androidapp/ui/home/HomeFragment.java
+++ frontend/app/src/main/java/com/example/androidapp/ui/home/HomeFragment.java
@@ -10,8 +10,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.androidapp.Adapter.MyPagerAdapter;
 import com.example.androidapp.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class HomeFragment extends Fragment {
 
@@ -19,16 +23,30 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        TabLayout tabLayout = root.findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("1"));
+        tabLayout.addTab(tabLayout.newTab().setText("2"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = root.findViewById(R.id.pager);
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getActivity().getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
         return root;
     }
 }
