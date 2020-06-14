60193d07016c3f89d5ff05fd2a31f38a7d335b18
wish142857 <wish142857@163.com>
2020-06-14 23:55:05 +0800

    update: finish intention in request

--- frontend/app/src/main/java/com/example/androidapp/util/Global.java
+++ frontend/app/src/main/java/com/example/androidapp/util/Global.java
@@ -4,6 +4,5 @@ public class Global {
     // *** HTTP ***
     public static final boolean HTTP_TEST_MODE = true;
     public static final boolean HTTP_DEBUG_MODE = true;
-    // public static final String SERVER_URL = "http://192.168.10.6:8000";
     public static final String SERVER_URL = "http://121.40.164.252:8080";
 }
