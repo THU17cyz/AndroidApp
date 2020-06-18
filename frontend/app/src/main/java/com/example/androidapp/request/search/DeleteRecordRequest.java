3c156ce03582e42a586bf1bcd8d4be584f08d68c
Yezhen Cong <52420115+THU17cyz@users.noreply.github.com>
2020-06-18 20:22:37 +0800

    update: 路径bug

--- frontend/app/src/main/java/com/example/androidapp/request/search/DeleteRecordRequest.java
+++ frontend/app/src/main/java/com/example/androidapp/request/search/DeleteRecordRequest.java
@@ -7,7 +7,7 @@ import okhttp3.Callback;
 public class DeleteRecordRequest extends BasePostRequest {
     public DeleteRecordRequest(Callback callback, String key) {
         // 设置请求URL
-        this.to("/api/intention/delete_search_record");
+        this.to("/api/search/delete_search_record");
         // 设置请求参数
         this.put("key", key);
         // 设置回调函数
